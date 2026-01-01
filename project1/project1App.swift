4091b61abe4a3ddcccc300ad067ae0ebad6685cc
belitra <berybelitra@gmail.com>
2026-01-01 09:09:04 -0700

    Initial Commit

---
+++ project1/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  project1
+//
+//  Created by Niyonkuru Belitra on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ project1Tests/project1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  project1Tests.swift
+//  project1Tests
+//
+//  Created by Niyonkuru Belitra on 1/1/26.
+//
+
+import Testing
+@testable import project1
+
+struct project1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
