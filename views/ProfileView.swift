6461236fac6d1eec172b0938047ff7ef331ed763
belitra <berybelitra@gmail.com>
2026-01-03 11:20:45 -0700

    Set up the app with tabs "Log Session" "History" "Stats" "Profile", ensured that app runs, and figured out the privacy and settings for develop app to appear on my phone.

---
+++ views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  project1
+//
+//  Created by Niyonkuru Belitra on 1/3/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
