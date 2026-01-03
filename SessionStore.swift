e61c2fca343d3fe617b3bb2b029d534b89be9eb4
belitra <berybelitra@gmail.com>
2026-01-03 13:36:36 -0700

    Added the main data sturucte (date, duration, location) by crearting a study session model and a session  SessionStore ObservableObject. The study session adds new session with cleaned inputs and calculates daily totals.

---
+++ SessionStore.swift
@@ -0,0 +1,8 @@
+//
+//  SessionStore.swift
+//  project1
+//
+//  Created by Niyonkuru Belitra on 1/3/26.
+//
+
+import Foundation
---
+++ StudySession.swift
@@ -0,0 +1,8 @@
+//
+//  StudySession.swift
+//  project1
+//
+//  Created by Niyonkuru Belitra on 1/3/26.
+//
+
+import Foundation
