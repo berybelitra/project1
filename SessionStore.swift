889db650d36a507be91178c6c5b4e83231d8c1a6
belitra <berybelitra@gmail.com>
2026-01-05 11:13:13 -0700

    Add study session models and fix xcode project setup

--- SessionStore.swift
+++ SessionStore.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+// Creating reference type to store data that auto refreshes
+final class SessionStore: ObservableObject {
+    // When the variable sessions changes, it updates all views using it
+    @Published var sessions: [StudySession] = []
+    // Adding a study session
+    func addSession(location: String?, duration: Int) {
+        // This allows input to clean spaces
+        // ex: " Library " -> "Library"
+        let cleanedLocation = location?
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+        // This makes empty inputs turn into nil
+        let locationValue =
+            (cleanedLocation?.isEmpty == true) ? nil : cleanedLocation
+        // Creates new study session object
+        let newSession = StudySession(
+            date: Date(),
+            duration: duration,
+            location: locationValue
+        )
+        // Inserts newest session at the top
+        sessions.insert(newSession, at: 0)
+    }
+    // Calculates total study duration for today and returns an Int
+    func totalMinutesToday() -> Int {
+        let calendar = Calendar.current
+
+        return sessions
+            .filter { calendar.isDateInToday($0.date) }
+            .map { $0.duration }
+            .reduce(0, +)
+    }
+}
--- StudySession.swift
+++ StudySession.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+// Represents one study session
+struct StudySession: Identifiable {
+    let id = UUID()          // unique identifier for SwiftUI lists
+    let date: Date           // when the session occurred
+    let duration: Int        // duration in minutes
+    let location: String?    // optional study location
+}
